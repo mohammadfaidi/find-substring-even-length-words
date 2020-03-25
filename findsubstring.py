5e4ef76c6ab4ff98595beaac3c1dc43c8b5da5a1
mohammad <mohamadfaidi@outlook.com>
2020-03-25 14:11:17 +0200

    findsubstring& evenlengthwords

---
+++ findsubstring.py
@@ -0,0 +1,11 @@
+
+def check():
+    str=input("Enter the sentence :")
+    str2=input("Enter the finder :")
+    if str.find(str2) == -1:
+        print("no")
+    else:
+        print("yes")
+
+while 1:
+    check()
